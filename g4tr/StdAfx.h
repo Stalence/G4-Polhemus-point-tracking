139a48922702e7ca8af5d002788561f65657c1f6
Stalence <ruecart@hotmail.com>
2017-04-23 03:12:17 +0300

    G4 tracker

---
+++ g4tr/StdAfx.h
@@ -0,0 +1,33 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__5194B7C1_3EF4_4FDE_8601_C75EE4D28B03__INCLUDED_)
+#define AFX_STDAFX_H__5194B7C1_3EF4_4FDE_8601_C75EE4D28B03__INCLUDED_
+
+#define  WINVER 0x0500 
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define _CRT_SECURE_NO_WARNINGS
+//#define _USE_32BIT_TIME_T
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#endif
+
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <tchar.h>
+#include <wincon.h>
+
+// TODO: reference additional headers your program requires here
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__5194B7C1_3EF4_4FDE_8601_C75EE4D28B03__INCLUDED_)
